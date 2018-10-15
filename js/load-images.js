7b34083c8d802cc7f84abdbdf55bb7af699408cd
Jacoby Clarke <Jacoby.work15@gmail.com>
2018-10-15 13:59:54 -0500

    fixed gallery
    commented out load-images.js

--- js/load-images.js
+++ js/load-images.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){
-    var dir = "../images/includes/";
-    var fileExt = ".jpg";
-    
-    $.ajax ({
-        url: dir,
-        success: function (data) {
-    $(data).find("a:contains(" + fileExt + ")").each(function () {
-// removes https header from file path
-            var filename = this.href.replace(window.location.host, "").replace("http:///deponce/novo-simplified/home/", "");
-        //apends image  to a-img classed div
-            $(".a-img").append("<img src='" + dir + filename + "'>");
-        });
-}
-                  })
-});
+//$(document).ready(function(){
+//    var dir = "../images/includes/";
+//    var fileExt = ".jpg";
+//    
+//    $.ajax ({
+//        url: dir,
+//        success: function (data) {
+//    $(data).find("a:contains(" + fileExt + ")").each(function () {
+//// removes https header from file path
+//            var filename = this.href.replace(window.location.host, "").replace("http:///deponce/novo-simplified/home/", "");
+//        //apends image  to a-img classed div
+//            $(".a-img").append("<img src='" + dir + filename + "'>");
+//        });
+//}
+//                  })
+//});
