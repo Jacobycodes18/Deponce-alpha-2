e4fbcdb153c41f2485a28d9e901a94c43efb0865
Jacoby Clarke <Jacoby.work15@gmail.com>
2018-11-05 10:40:05 -0600

    fixed text on home carousel for mobile sizes

--- auth/post.php
+++ auth/post.php
@@ -12,7 +12,6 @@
             $id = '1234567890';
             $id = str_shuffle($id);
             $id = substr($id, 0, 10);
-//            $pid = 'dpn' + $id; 
 
 
             $sql = "INSERT INTO posts (post_title, post_subtitle, post_id, post_img, post_content) VALUES ('$title', '$sub', 'dpn$id', '$imgUrl','$content');";
@@ -20,6 +19,7 @@
             $ran = mysqli_query($conn, $sql);
             
             if($ran) {
+//                add date
                 $sql = "UPDATE posts SET post_date = DATE_ADD(NOW(), INTERVAL 10 MINUTE) WHERE post_title = '$title'";
                 mysqli_query($conn, $sql);
                 echo 'posted';
