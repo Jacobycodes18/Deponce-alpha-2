0bad7cf9be35cbf7f4c36d875fc6afee8e87b4bf
Jacoby Clarke <Jacoby.work15@gmail.com>
2018-11-05 11:01:08 -0600

    Post changes
    -added after making post redirect
    -added empty fields validation

--- auth/post.php
+++ auth/post.php
@@ -8,26 +8,26 @@
         $imgUrl = mysqli_real_escape_string($conn, $_POST['image']);
         $content = mysqli_real_escape_string($conn, $_POST['content']);
         
-        if(!empty($title) || !empty($sub) || !empty($imgUrl) || !empty($content)) {
-            $id = '1234567890';
-            $id = str_shuffle($id);
-            $id = substr($id, 0, 10);
-
-
-            $sql = "INSERT INTO posts (post_title, post_subtitle, post_id, post_img, post_content) VALUES ('$title', '$sub', 'dpn$id', '$imgUrl','$content');";
-            
-            $ran = mysqli_query($conn, $sql);
-            
-            if($ran) {
-//                add date
-                $sql = "UPDATE posts SET post_date = DATE_ADD(NOW(), INTERVAL 10 MINUTE) WHERE post_title = '$title'";
-                mysqli_query($conn, $sql);
-                echo 'posted';
-            } else {
-                echo'didnt run';
-            }
-        } else {
+        if(empty($title) || empty($sub) || empty($imgUrl) || empty($content)) {
             echo 'all fields must be filled';
+            } else {
+                    $id = '1234567890';
+                    $id = str_shuffle($id);
+                    $id = substr($id, 0, 10);
+
+
+                    $sql = "INSERT INTO posts (post_title, post_subtitle, post_id, post_img, post_content) VALUES ('$title', '$sub', 'dpn$id', '$imgUrl','$content');";
+
+                    $ran = mysqli_query($conn, $sql);
+
+                    if($ran) {
+        //                add date
+                        $sql = "UPDATE posts SET post_date = DATE_ADD(NOW(), INTERVAL 10 MINUTE) WHERE post_title = '$title'";
+                        mysqli_query($conn, $sql);
+                        echo 'posted';
+                        sleep(5);
+                        header("Location: ../news?posted=true");
+                            }
         }
     } else {
         echo 'error';
